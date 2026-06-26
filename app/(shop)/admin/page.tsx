3717f99597885123b24bb62493c5c5550fe5e29b
leuuis <louis.anime@gmail.com>
2026-06-25 18:20:04 -0600

    Initial commit with routes and basic layouts

---
+++ app/(shop)/admin/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CategoryPage() {
+    return (
+        <div>
+            <h1>Hello Admin Page</h1>
+        </div>
+    );
+}
---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "../../config/fonts";
+
+export default function SignIn() {
+  return (
+    <div className="">
+      <h1 className={`${titleFont.className} font-bold`}>Sign In Page</h1>
+    </div>
+  );
+}
