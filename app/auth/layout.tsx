c0c1c0d6de89e2592a31434a7b321e763d4db439
leuuis <louis.anime@gmail.com>
2026-06-27 17:39:10 -0600

    Top menu added.

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -5,7 +5,7 @@ export default function ShopLayout({
     children: React.ReactNode;
 }) {
     return (
-        <main className="bg-gray-500 min-h-screen">
+        <main className="min-h-screen">
             {children}
         </main>
     );
---
+++ components/ui/top-menu/TopMenu.tsx
@@ -0,0 +1,43 @@
+import { titleFont } from "@/app/config/fonts"
+import Link from "next/link"
+import { IoCartOutline, IoMenuOutline, IoSearchOutline } from "react-icons/io5"
+
+export const TopMenu = () => {
+  return (
+    <nav className="flex px-5 justify-between items-center w-full">
+
+      {/* Logo */}
+      <div>
+        <Link href="/">
+          <span className={`${titleFont.className} antialiased font-bold`}>Innova</span>
+          <span className="text-sm text-gray-500"> | Shop</span>
+        </Link>
+      </div>
+
+      {/* Menu */}
+      <div className="hidden sm:block">
+        <Link href="category/men" className="m-2 p-2 rounded-md transition-all hover:bg-gray-200 text-sm text-gray-500">Men</Link>
+        <Link href="category/women" className="m-2 p-2 rounded-md transition-all hover:bg-gray-200 text-sm text-gray-500">Women</Link>
+        <Link href="category/kids" className="m-2 p-2 rounded-md transition-all hover:bg-gray-200 text-sm text-gray-500">Kids</Link>
+      </div>
+
+      {/* Search, Cart, Menu */}
+      <div className="flex items-center">
+        <Link href="/search" className="mx-2">
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+        <div className="relative">
+          <span className="absolute text-xs rounded-full px-1 font-bold top-3 -right-2   bg-blue-700 text-white">3</span>
+          <Link href="/cart" className="mx-2">
+            <IoCartOutline className="w-5 h-5" />
+          </Link>
+        </div>
+
+        <button className="mx-2 rounded-md transition-all hover:bg-gray-100 p-2">
+          Menu
+        </button>
+      </div>
+
+    </nav>
+  )
+}
